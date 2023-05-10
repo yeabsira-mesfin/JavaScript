279ef4afdf3e2aebce4f4f62f42b430586d45c5f
Yeabsira Mesfin <72326656+yeabsira-mesfin@users.noreply.github.com>
2023-05-10 16:15:51 -0400

    Introduction

---
+++ Introduction.js
@@ -0,0 +1,39 @@
+// You can use back to insert values in your display statements. For example
+let yeabsira='Mesfin'
+let mesfin='Yeabsira'
+
+console.log(`My name is ${mesfin} ${ yeabsira}`);
+
+// Undefined = Value is not assigned
+// null = empty
+
+//typeof operator returns the type of the operand. For example
+
+console.log(typeof(yeabsira))
+
+// Modal means the user can not access the window unless they deal with the modal window.
+// Alert Shows a message and waits for the usert to press ok.
+
+//// alert('Hello, world')
+
+
+// Prompt(title,[default]) 
+// title is a text or anything you can pass to the modal window.
+// default is anything you can use as an input.
+
+
+//// let age = prompt('How old are you',23);
+//// alert(`You are ${age} years old`);
+
+// confirm appears with two buttons, ok and cancel.
+// ok returns true and cancels return false.
+
+//// let isTeacher = confirm('Are you a teacher?');
+//// alert(isTeacher);
+
+// xx is exponentiation operator.
+
+//// let a = 5;
+//// let b = 5;
+//// console.log(a**b)
+
